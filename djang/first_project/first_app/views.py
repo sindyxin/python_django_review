99d47f4aa5bf6948af8bde0a0fd858f2d2da2c5b
Sindy <sindyxin1@gmail.com>
2018-08-16 16:20:24 -0700

    practice django models and templates and populate script for dummy data

--- djang/first_project/first_app/views.py
+++ djang/first_project/first_app/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request_banana):
-  return HttpResponse('Hello World')
+  my_dict = {'insert_me': "Hello I am from views.py!"}
+
+  return render(request_banana, 'first_app/index.html', context=my_dict)
