54d44d0c5f13e17235cdca210aac08f4eba0bdb9
Sindy <sindyxin1@gmail.com>
2018-08-16 16:23:09 -0700

    git all changes for now

--- djang/ProTwo/appTwo/views.py
+++ djang/ProTwo/appTwo/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
 def index(request):
-  return HttpResponse("<em>Second Project</em>")
+  return HttpResponse("<em>Hello this is second app</em>")
+def help(request):
+  my_dict ={
+    "help": "This  is help page"
+  }
+  return render (request, "appTwo/help.html", context=my_dict)
